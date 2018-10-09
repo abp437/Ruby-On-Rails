07b7addb105e62326c912e152af9bd919202eb04
abp437 <abp437@gmail.com>
2018-10-09 21:07:36 +0530

    Users App added all others deleted

--- myarticles/app/models/article.rb
+++
@@ -1,6 +0,0 @@
-class Article < ApplicationRecord
-# This just means that the 'title' of each article is required
-  validates :title, presence: true
-# This just means that the 'body' of each article is required
-  validates :body, presence: true
-end
